395e82dbab97c55f18ec8657038fdf05e7401f03
Kirill <khvorikov.k@yandex.ru>
2021-08-23 21:52:37 +0300

    inint

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Header slider =====================================
+const swiper_header = new Swiper('.swiper-container', {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+
+
+
